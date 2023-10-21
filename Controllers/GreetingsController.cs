a22682efb93d7975ef209621bce17e417f977393
Christoph Kopf <ckopf86@gmail.com>
2023-10-21 21:33:17 +0200

    add greetings controller

---
+++ Controllers/GreetingsController.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
+namespace dotnet_greet.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class GreetingsController : ControllerBase
+{
+    private readonly ILogger<GreetingsController> _logger;
+    private readonly GreetOptions _greetOptions;
+
+    public GreetingsController(
+        ILogger<GreetingsController> logger,
+        IOptions<GreetOptions> greetOptions) 
+    {
+        _logger = logger;
+        _greetOptions = greetOptions.Value;
+    }
+
+    [HttpGet(Name = "GreetMe")]
+    public string GreetMe()
+    {
+        return $"Hello {_greetOptions.Who}, you're using {_greetOptions.From} environment";
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,6 +7,10 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.Configure<dotnet_greet.Controllers.GreetOptions>(
+    builder.Configuration.GetSection(
+        key: nameof(dotnet_greet.Controllers.GreetOptions)));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
