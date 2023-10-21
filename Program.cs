c0c547af72699d41a397ecbdf4c7d9867db295a4
Christoph Kopf <ckopf86@gmail.com>
2023-10-22 00:47:57 +0200

    playing around with environments

--- Program.cs
+++ Program.cs
@@ -21,7 +21,7 @@ builder.Services.Configure<dotnet_greet.Controllers.GreetOptions>(
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (!app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
