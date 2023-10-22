2f0b3e97a42e05b0103eb64c99f828c71cafed35
Christoph Kopf <ckopf86@gmail.com>
2023-10-22 22:02:52 +0200

    add test library

---
+++ Objectbay.Vacation.WebApi.Tests/BasicTests.cs
@@ -0,0 +1,41 @@
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Objectbay.Vacation.WebApi.Tests;
+
+public class BasicTests 
+    : IClassFixture<WebApplicationFactory<Program>>
+{
+    private readonly WebApplicationFactory<Program> _factory;
+
+    public BasicTests(WebApplicationFactory<Program> factory)
+    {
+        _factory = factory;
+    }
+
+    [Fact]
+    public void Spring_should_be_cool()
+    {
+        "spring".Should().Be("cool");
+    }
+
+    [Theory]
+    [InlineData("/greetings")]
+    public async Task Get_Greet(string url)
+    {
+        // Arrange
+        var client = _factory.CreateClient();
+
+        // Act
+        var response = await client.GetAsync(url);
+
+        // Assert
+        response.EnsureSuccessStatusCode(); // Status Code 200-299
+
+        var contnetType = response?.Content?.Headers?.ContentType?.ToString();
+        string content = await (response?.Content?.ReadAsStringAsync() ?? Task.FromResult(""));
+
+        contnetType.Should().StartWith("text/plain");
+        content.Should().StartWith("Hello").And.Contain(", you're using");
+    }
+}
