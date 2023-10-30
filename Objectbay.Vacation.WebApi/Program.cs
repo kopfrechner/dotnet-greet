4b9723e2ecb0a677f46f8662522ce5eb5586f909
Christoph Kopf <christoph.kopf@objectbay.com>
2023-10-30 22:17:21 +0100

    expose enum as string

--- Objectbay.Vacation.WebApi/Program.cs
+++ Objectbay.Vacation.WebApi/Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(
+    o => o.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter())
+);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -20,6 +23,7 @@ builder.Services.Configure<Objectbay.Vacation.WebApi.Controllers.GreetOptions>(
 
 var app = builder.Build();
 
+// Apply migrations on startup
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
