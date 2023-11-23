75b3e545d1cbac34a22a1badf714fc043c72c02b
Christoph Kopf <christoph.kopf@objectbay.com>
2023-11-23 20:01:46 +0100

    add seed

--- Objectbay.Vacation.WebApi/DbModels/ApplicationDbContext.cs
+++ Objectbay.Vacation.WebApi/DbModels/ApplicationDbContext.cs
@@ -7,4 +7,22 @@ public class ApplicationDbContext : DbContext
        : base(options) { }
 
     public DbSet<VacationItem> VacationItems { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<VacationItem>().HasData(
+            new VacationItem
+            {
+                Id = Guid.Parse("3fb362d5-18c3-45de-813e-6147adbc3d9a"),
+                Name = "Badehose",
+                Category = VacationItemCategory.CLOTHING
+            },
+            new VacationItem
+            {
+                Id = Guid.Parse("c37f373d-6869-48a2-a8c0-2e3b2626da6a"),
+                Name = "Sonnencreme",
+                Category = VacationItemCategory.SURVIVAL_ESSENTIALS
+            }
+        );
+    }
 }
---
+++ Objectbay.Vacation.WebApi/Migrations/20231123182908_seed.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Objectbay.Vacation.WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class seed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "VacationItems",
+                columns: new[] { "Id", "Category", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("3fb362d5-18c3-45de-813e-6147adbc3d9a"), 0, "Badehose" },
+                    { new Guid("c37f373d-6869-48a2-a8c0-2e3b2626da6a"), 3, "Sonnencreme" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "VacationItems",
+                keyColumn: "Id",
+                keyValue: new Guid("3fb362d5-18c3-45de-813e-6147adbc3d9a"));
+
+            migrationBuilder.DeleteData(
+                table: "VacationItems",
+                keyColumn: "Id",
+                keyValue: new Guid("c37f373d-6869-48a2-a8c0-2e3b2626da6a"));
+        }
+    }
+}
